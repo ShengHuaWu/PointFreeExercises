e84eb0c62f1bc0d7e34338e609f0ce3ee0d49171
ShengHua Wu <fantasy0404@gmail.com>
2018-11-06 21:33:35 +0100

    Append randomness exercise.

--- DecodableRandomness.playground/Contents.swift
+++ DecodableRandomness.playground/Contents.swift
@@ -144,11 +144,123 @@ try Date.init(from: a)
 try Date?.init(from: a)
 
 struct User: Decodable {
-    let id: Int
+    let id: UUID
     let name: String
     let email: String
 }
 
-print(try User.init(from: a))
-print(try User.init(from: a))
-print(try User.init(from: a))
+struct Gen<A> {
+    let run: () -> A
+}
+
+import Darwin
+let random = Gen(run: arc4random)
+
+extension Gen {
+    func map<B>(_ f: @escaping (A) -> B) -> Gen<B> {
+        return Gen<B> { f(self.run()) }
+    }
+}
+
+let uint64 = Gen<UInt64> {
+    let lower = UInt64(random.run())
+    let upper = UInt64(random.run()) << 32
+    return lower + upper
+}
+
+func int(in range: ClosedRange<Int>) -> Gen<Int> {
+    return Gen<Int> {
+        var delta = UInt64(truncatingIfNeeded: range.upperBound &- range.lowerBound)
+        if delta == UInt64.max {
+            return Int(truncatingIfNeeded: uint64.run())
+        }
+        delta += 1
+        let tmp = UInt64.max % delta + 1
+        let upperBound = tmp == delta ? 0 : tmp
+        var random: UInt64 = 0
+        repeat {
+            random = uint64.run()
+        } while random < upperBound
+        return Int(
+            truncatingIfNeeded: UInt64(truncatingIfNeeded: range.lowerBound)
+                &+ random % delta
+        )
+    }
+}
+
+func element<A>(of xs: [A]) -> Gen<A?> {
+    return int(in: 0...(xs.count - 1)).map { idx in
+        guard !xs.isEmpty else { return nil }
+        return xs[idx]
+    }
+}
+
+extension Gen {
+    func array(count: Gen<Int>) -> Gen<[A]> {
+        return Gen<[A]> {
+            Array(repeating: (), count: count.run())
+                .map { self.run() }
+        }
+    }
+}
+
+func uint8(in range: ClosedRange<UInt8>) -> Gen<UInt8> {
+    return int(in: Int(range.lowerBound) ... Int(range.upperBound)).map(UInt8.init)
+}
+
+let string = uint8(in: .min ... .max)
+    .map(UnicodeScalar.init)
+    .map(String.init)
+    .array(count: int(in: 0...100))
+    .map{ $0.joined() }
+
+let alpha = element(of: Array("abcdefghijklmnopqrstuvxcyz")).map{ $0! }
+
+extension Gen where A == Character {
+    func string(count: Gen<Int>) -> Gen<String> {
+        return map(String.init).array(count: count).map{ $0.joined() }
+    }
+}
+
+let randomID = int(in: 1 ... 1_000)
+
+let namePart = alpha.string(count: int(in: 4...8))
+let capitalNamePart = namePart.map{ $0.capitalized }
+let randomName = Gen<String> { capitalNamePart.run() + " " + capitalNamePart.run() }
+let randomEmail = Gen<String> { namePart.run() + "." + namePart.run() + "@gmail.com" }
+
+func zip2<A, B>(_ ga: Gen<A>, _ gb: Gen<B>) -> Gen<(A, B)> {
+    return Gen {
+        (ga.run(), gb.run())
+    }
+}
+
+func zip2<A, B, C>(with f: @escaping (A, B) -> C) -> (Gen<A>, Gen<B>) -> Gen<C> {
+    return { ga, gb in
+        zip2(ga, gb).map(f)
+    }
+}
+
+func zip3<A, B, C>(_ ga: Gen<A>, _ gb: Gen<B>, _ gc: Gen<C>) -> Gen<(A, B, C)> {
+    return zip2(ga, zip2(gb, gc)).map{ ($0, $1.0, $1.1) }
+}
+
+func zip3<A, B, C, D>(with f: @escaping (A, B, C) -> D) -> (Gen<A>, Gen<B>, Gen<C>) -> Gen<D> {
+    return { zip3($0, $1, $2).map(f) }
+}
+
+let hex = element(of: Array("0123456789ABCDEF")).map{ $0! }
+let uuidString = Gen<String> {
+    hex.string(count: .init { 8 }).run()
+        + "-" + hex.string(count: .init { 4 }).run()
+        + "-" + hex.string(count: .init { 4 }).run()
+        + "-" + hex.string(count: .init { 4 }).run()
+        + "-" + hex.string(count: .init { 12 }).run()
+}
+let randomUUID = uuidString.map(UUID.init).map{ $0! }
+
+let randomUser = zip3(with: User.init)(randomUUID, randomName, randomEmail)
+
+print(randomUser.run())
+print(randomUser.run())
+print(randomUser.run())
