e305b5ee785513a99ab9d015e34ae656beee07e3
ShengHua Wu <fantasy0404@gmail.com>
2018-11-30 22:13:51 +0100

    Append exercise to two flat map.

--- ATaleOfTwoFlatMap.playground/Contents.swift
+++ ATaleOfTwoFlatMap.playground/Contents.swift
@@ -11,3 +11,9 @@ cvv
     .flatMap { $0.split(separator: ",") }
     .compactMap { Int($0) }
     .reduce(0, +)
+
+// The difference between `map` & `flatMap` on optional
+// is `flatMap` unwraps the value but `map` doesn't
+let int1: Int? = 5
+let f = int1.flatMap { _ in Optional(10) } // f is Int?
+let m = int1.map { _ in Optional(10) } // m is Int??
