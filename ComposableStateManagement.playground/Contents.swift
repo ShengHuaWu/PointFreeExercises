5f488ea95a75587d3990e5a912e2617bc993e52c
ShengHua Wu <fantasy0404@gmail.com>
2019-11-03 13:46:12 +0100

    Append exercies for composable state management.

--- ComposableStateManagement.playground/Contents.swift
+++ ComposableStateManagement.playground/Contents.swift
@@ -25,3 +25,63 @@ func combine<State, Action>(_ reducers: (inout State, Action) -> Void...) -> (in
         reducers.forEach { $0(&state, action) }
     }
 }
+
+
+import Foundation
+import Combine
+
+struct AppState {
+  var count = 0
+  var favoritePrimes: [Int] = []
+  var activityFeed: [Activity] = []
+  var loggedInUser: User? = nil
+
+  struct Activity {
+    let type: ActivityType
+    let timestamp = Date()
+
+    enum ActivityType {
+      case addedFavoritePrime(Int)
+      case removedFavoritePrime(Int)
+    }
+  }
+
+  struct User {
+    let id: Int
+    let name: String
+    let bio: String
+  }
+}
+
+final class Store<Value, Action>: ObservableObject {
+    @Published var value: Value
+    let reducer: (inout Value, Action) -> Void
+
+    init(initialValue: Value, reducer: @escaping (inout Value, Action) -> Void) {
+        self.value = initialValue
+        self.reducer = reducer
+    }
+    
+    func send(_ action: Action) {
+        self.reducer(&self.value, action)
+    }
+}
+
+enum CounterAction {
+  case decrTapped
+  case incrTapped
+}
+
+func counterReducer(state: inout AppState, action: CounterAction) {
+  switch action {
+  case .decrTapped:
+    state.count -= 1
+  case .incrTapped:
+    state.count += 1
+  }
+}
+
+var state = AppState()
+counterReducer(state: &state, action: .incrTapped)
+counterReducer(state: &state, action: .decrTapped)
+print(state)
